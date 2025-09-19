44705f7b922b2d466a40ef2182165ceaa750ed01
puntodetalle2019-netizen <puntodetalle2019@gmail.com>
2025-09-19 09:21:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const div=document.querySelector('.cuadros');
 function cambiarcolor(e){
     e.target.style.backgroundColor="blue";
+
 }
+function volvercolor(e){
+    e.target.style.backgroundColor="yellow";
+
+}
+
+
 div.addEventListener('pointerdown',cambiarcolor);
+div.addEventListener('pointerup',volvercolor);
